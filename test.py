6d00d0ab8f61fb5e81945d32720bbe695fcc001d
OussemaJbeli <jbelioussema33@gmail.com>
2025-02-28 14:17:18 +0100

    test

--- test.py
+++ test.py
@@ -8,21 +8,60 @@ class w3schooldsTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()  # Selenium will find ChromeDriver in PATH
 
+# test connection
     def test_website_title(self):
         driver = self.driver
-        driver.get("https://www.w3schools.com/")
-        print('first print with TITLE : ',driver.title)
-        self.assertIn("W3Schools", driver.title)
-
-    def test_search_bar(self):
+        driver.get("https://boontest.netlify.app/")
+        self.assertIn("BOON ", driver.title)
+        time.sleep(5)
+# test open sidebar
+    def test_opensidebar(self):
         driver = self.driver
-        driver.get("https://www.w3schools.com/")
-        search = driver.find_element(By.ID, 'tnb-google-search-input')
-        search.send_keys("python")
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'openSideBar')
         search.send_keys(Keys.RETURN)
-        time.sleep(15)
-        self.assertIn("Python", driver.title)
-        assert "Python" in driver.page_source
+        self.assertIn("open sidebar")
+        time.sleep(2)
+# test change language
+    def test_changeLanguage(self):
+        driver = self.driver
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'languageButton')
+        search.send_keys(Keys.RETURN)
+        self.assertIn("open sidebar")
+        time.sleep(2)
+
+    def test_selectArabic(self):
+        driver = self.driver
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'arabicSwitsh')
+        search.send_keys(Keys.RETURN)
+        self.assertIn("open sidebar")
+        time.sleep(2)
+# test change mode
+    def test_changeMode(self):
+        driver = self.driver
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'modeButton')
+        search.send_keys(Keys.RETURN)
+        self.assertIn("open sidebar")
+        time.sleep(2)
+
+    def test_selectDark(self):
+        driver = self.driver
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'darkModeSwitsh')
+        search.send_keys(Keys.RETURN)
+        self.assertIn("open sidebar")
+        time.sleep(2)
+# test close didebar
+    def test_closesidebar(self):
+        driver = self.driver
+        driver.get("https://boontest.netlify.app/")
+        search = driver.find_element(By.ID, 'closeSideBar')
+        search.send_keys(Keys.RETURN)
+        self.assertIn("open sidebar")
+        time.sleep(2)
 
     def tearDown(self):
         self.driver.quit()
